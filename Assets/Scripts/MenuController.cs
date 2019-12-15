a7640ebf7bca7fafa416f3d7a4146f9bef7928ca
alvinbr <alvinbr88@gmail.com>
2019-12-15 17:02:55 +0700

    menambah beberapa scene

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void OnPlay(){
+        SceneManager.LoadScene("Play");
+    }
+    public void OnBack(){
+        SceneManager.LoadScene("Menu");
+    }
+    public void OnHelp(){
+        SceneManager.LoadScene("Help");
+    }
+    public void OnCredits(){
+        SceneManager.LoadScene("Credits");
+    }
+    public void OnSetting(){
+        SceneManager.LoadScene("Setting");
+    }
+    public void OnExit(){
+        SceneManager.LoadScene("Exit");
+    }
+    public void OnStage1(){
+        SceneManager.LoadScene("Stage1");
+    }
+    public void OnStage2(){
+        SceneManager.LoadScene("Stage2");
+    }
+    public void OnStage3(){
+        SceneManager.LoadScene("Stage3");
+    }
+    // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
