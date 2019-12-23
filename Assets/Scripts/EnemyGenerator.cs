8148e1cc1bfe8dd8fb7357f1085ab32fceb10a28
anzalsyarifurrozaqi <anzalsya1@gmail.com>
2019-12-23 10:53:22 +0700

    memperbaiki bug enemy

--- Assets/Scripts/EnemyGenerator.cs
+++ Assets/Scripts/EnemyGenerator.cs
@@ -3,13 +3,14 @@
 public class EnemyGenerator : MonoBehaviour
 {
     public static EnemyGenerator instantiate;
-    GameObject[] enemys;
+    public GameObject[] enemys;
     GameObject player;
     float[] objectsDistance;
     public float distance;
     public bool die = false;
     public int i;
     int j = 0;
+    int enemyLenght;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,29 +19,48 @@ public class EnemyGenerator : MonoBehaviour
             instantiate = this;
         }
         enemys = GameObject.FindGameObjectsWithTag("Enemy");
-        player = GameObject.FindWithTag("Player");
-        i = 0;
+        player = GameObject.FindWithTag("Player");        
         objectsDistance = new float[enemys.Length];
+        enemyLenght = enemys.Length - 1;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        //Debug.Log(enemys.Length);
+        
         //Debug.Log("i " + i);
         //Debug.Log("j " + j);
         //Debug.Log(enemys[i].transform.position.x);
         getStatus();
-        
-        if (player.transform.position.x < enemys[i].transform.position.x)
+        Debug.Log(enemyLenght);        
+        i = 0;
+        for (int temp = 0; temp < enemys.Length; temp++)
+        {            
+            if (enemys[temp] != null)
+            {
+                if (enemys[i] == null)
+                {
+                    i = temp;
+                }
+                if (enemys[i].transform.position.x > enemys[temp].transform.position.x)
+                {
+                    i = temp;
+                }                
+            }            
+        }        
+        Debug.Log("i" + i);
+        if (enemys[i] != null)
         {
-            objectsDistance[j] = Mathf.Abs(enemys[i].transform.position.x - player.transform.position.x);
-        }
-        else
-        {
-            objectsDistance[j] = Mathf.Abs(player.transform.position.x - enemys[i].transform.position.x);
-        }
-        
+            if (player.transform.position.x < enemys[i].transform.position.x)
+            {
+                objectsDistance[j] = Mathf.Abs(enemys[i].transform.position.x - player.transform.position.x);
+            }
+            else
+            {
+                objectsDistance[j] = Mathf.Abs(player.transform.position.x - enemys[i].transform.position.x);
+            }
+            
+        }                                
         getDistance();
     }
 
@@ -53,9 +73,9 @@ public class EnemyGenerator : MonoBehaviour
    void getStatus()
     {
         //Debug.Log(die);
+        //Debug.Log(enemys.Length);
         if (die)
-        {
-            i += 1;
+        {            
             j += 1;
             die = false;
         }
